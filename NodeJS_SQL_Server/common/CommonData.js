ddb863f0fca9a3420b73fcd4f1c659777fbdc6fd
Thanh Binh <nguyenthanhbinh.23122000@gmail.com>
2021-02-18 05:11:36 +0700

    binh commit

---
+++ NodeJS_SQL_Server/common/CommonData.js
@@ -0,0 +1,9 @@
+const config = {
+    DB_NAME = '103.130.215.232',
+    USER_NAME = 'sa',
+    PASSWORD = 'sa!123456'
+}
+
+module.exports = {
+    config
+}
--- NodeJS_SQL_Server/database/database.js
+++ NodeJS_SQL_Server/database/database.js
@@ -1,1 +1,26 @@
-const Sequelize = require('')
+const Sequelize = require('sequelize')
+const {config} = require('../common/CommonData')
+
+const sequelize = new Sequelize(
+    config.DB_NAME,
+    config.USER_NAME,
+    config.PASSWORD,
+    {        
+        dialect: 'mssql',
+        host: 'localhost',
+        operatorsAliases: false,
+        pool: {
+            max: 5,
+            min: 0,
+            require: 30000,
+            idle: 10000,            
+        }
+    }
+);
+
+const Op = Sequelize.Op
+
+module.exports = {
+    sequelize,
+    Op
+}
